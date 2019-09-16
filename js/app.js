846a902fdbb8805db57e8743d11dc4a7846e4da4
Korawit Prutsachainimmit <korawit.p@gmail.com>
2019-09-16 23:51:45 +0700

    Init

---
+++ js/app.js
@@ -0,0 +1,12 @@
+document.addEventListener('init', function (event) {
+  var page = event.target;
+
+
+  if (page.id === 'homePage') {
+    console.log("homePage");
+
+    $("#menubtn").click(function () {
+      $("#sidemenu")[0].open();      
+    });
+  }
+});
