6a87638e97156b25f34eaaaf661d99be795f2d46
Korawit Prutsachainimmit <korawit.p@gmail.com>
2019-09-17 16:46:25 +0700

    Added firebase support

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDAlab9BnsyOOMiKOcSlcgvuPDwE9ppc2Y",
+  authDomain: "food247-5905e.firebaseapp.com",
+  databaseURL: "https://food247-5905e.firebaseio.com",
+  projectId: "food247-5905e",
+  storageBucket: "food247-5905e.appspot.com",
+  messagingSenderId: "526310084175",
+  appId: "1:526310084175:web:6a7000ed289e0fb38de43a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
+
 document.addEventListener('init', function (event) {
   var page = event.target;
 
@@ -6,7 +21,19 @@ document.addEventListener('init', function (event) {
     console.log("homePage");
 
     $("#menubtn").click(function () {
-      $("#sidemenu")[0].open();      
+      $("#sidemenu")[0].open();
+    });
+
+    $("#carousel").empty();
+    db.collection("recommended").get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {       
+        var item = `<ons-carousel-item modifier="nodivider" id="item${doc.data().id}" class="recomended_item">
+            <div class="thumbnail" style="background-image: url('${doc.data().photoUrl}')">
+            </div>
+            <div class="recomended_item_title" id="item1_${doc.data().id}">${doc.data().name}</div>
+        </ons-carousel-item>`
+        $("#carousel").append(item);
+      });
     });
   }
 
@@ -14,13 +41,13 @@ document.addEventListener('init', function (event) {
     console.log("menuPage");
 
     $("#login").click(function () {
-      $("#content")[0].load("login.html");  
-      $("#sidemenu")[0].close();   
+      $("#content")[0].load("login.html");
+      $("#sidemenu")[0].close();
     });
 
     $("#home").click(function () {
-      $("#content")[0].load("home.html");  
-      $("#sidemenu")[0].close();   
+      $("#content")[0].load("home.html");
+      $("#sidemenu")[0].close();
     });
   }
 
@@ -28,7 +55,7 @@ document.addEventListener('init', function (event) {
     console.log("loginPage");
 
     $("#backhomebtn").click(function () {
-      $("#content")[0].load("home.html");      
+      $("#content")[0].load("home.html");
     });
   }
 });
