9e615acf9de014f46797c6220413c816691ee8a1
Korawit Prutsachainimmit <korawit.p@gmail.com>
2019-10-19 00:05:30 +0700

    Added category page

--- js/app.js
+++ js/app.js
@@ -20,13 +20,23 @@ document.addEventListener('init', function (event) {
   if (page.id === 'homePage') {
     console.log("homePage");
 
+    $("#thaibtn").click(function () {
+      localStorage.setItem("selectedCategory", "thai");
+      $("#content")[0].load("category.html");
+    });
+
+    $("#drinkbtn").click(function () {
+      localStorage.setItem("selectedCategory", "drink");
+      $("#content")[0].load("category.html");
+    });
+
     $("#menubtn").click(function () {
       $("#sidemenu")[0].open();
     });
 
     $("#carousel").empty();
     db.collection("recommended").get().then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {       
+      querySnapshot.forEach((doc) => {
         var item = `<ons-carousel-item modifier="nodivider" id="item${doc.data().id}" class="recomended_item">
             <div class="thumbnail" style="background-image: url('${doc.data().photoUrl}')">
             </div>
@@ -49,6 +59,37 @@ document.addEventListener('init', function (event) {
       $("#content")[0].load("home.html");
       $("#sidemenu")[0].close();
     });
+  }
+
+  if (page.id === 'categoryPage') {
+    var category = localStorage.getItem("selectedCategory");
+    console.log("categoryPage:" + category);
+
+    $("#header").html(category);
+
+    $("#menubtn").click(function () {
+      $("#sidemenu")[0].open();
+    });
+
+    $("#list").empty();
+    db.collection("recommended").where("category", "==", category).get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        var item = `<ons-row class="category">
+                <ons-col modifier="nodivider">
+                    <div class="category_header" style="background-image: url('${doc.data().photoUrl}')">
+                        <figure class="category_thumbnail" id="thaibtn">
+                            <div class="category_title" id="Category_1_name">${doc.data().name}</div>
+                        </figure>
+                    </div>
+                </ons-col>
+         </ons-row>`
+        $("#list").append(item);
+        console.log(doc.data().name);
+        
+      });
+    });
+
   }
 
   if (page.id === 'loginPage') {
